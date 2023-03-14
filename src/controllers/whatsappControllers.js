b6cc81a0f1977ad8c0ab1281704df1da18e9905d
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2023-03-14 15:34:58 -0300

    feat: função validadora de token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,12 +1,25 @@
 const VerifyToken = (req, res) => {
-    res.send("Hello verifyToken")
+  console.log(req.query)
+  try {
+    var accessToken = "RTQWWTVHBDEJHJKIKIKNDS9090DS";
+      var token = req.query["hub.verify_token"];
+      var challenge = req.query["hub.challenge"];
+
+      if(challenge != null && token != null && token == accessToken){
+          res.send(challenge);
+      }else{
+          res.status(400).send();
+      }
+  } catch (error) {
+    res.status(400).send()
+  }
 }
 
 const ReceivedMessage = (req, res) => {
-    res.send("Hello ReceivedMessage")
+  res.send("Hello ReceivedMessage")
 }
 
 module.exports = {
-    VerifyToken,
-    ReceivedMessage
+  VerifyToken,
+  ReceivedMessage
 }
